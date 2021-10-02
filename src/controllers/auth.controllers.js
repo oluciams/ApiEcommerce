20eb0299b817a1c71adbea6c9fb51e37b775fad5
olga <oluciams84@gmail.com>
2021-10-02 10:10:24 -0500

    ningun cambio

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -14,8 +14,8 @@ const createUser = async (req, res) => {
     const user = await new User({ name, lastname, email, password, profilePicture });
     await user.save();
     res.status(201).json(user);
-  } catch (error) {
-    res.status(400).json(error) 
+  } catch (error) {   
+    res.status(400).json(error)    
   }
 }
 
