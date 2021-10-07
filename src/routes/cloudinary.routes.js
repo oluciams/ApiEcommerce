847efacefd60e852dd96965504bfc497c2780779
olga <oluciams84@gmail.com>
2021-10-07 14:35:40 -0500

    Agrega ruta y controller para eliminar imagen de cloudinary

--- src/routes/cloudinary.routes.js
+++ src/routes/cloudinary.routes.js
@@ -1,9 +1,11 @@
 const router = require('express').Router()
-const {getImages, uploadImages} = require('../controllers/cloudinary.controllers')
+const {getImages, uploadImages, deleteImages} = require('../controllers/cloudinary.controllers')
 
 router.get('/api/images', getImages)
 
 router.post('/api/upload', uploadImages)
+
+router.delete('/api/images/ecommerce/:id', deleteImages)
 
 
 module.exports = router
